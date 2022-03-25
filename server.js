cec835a59f34597b8e3ce11038de6fa9e966b763
Sean-Thomo <seansthomo@gmail.com>
2022-03-25 13:58:22 +0200

    initial commmit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+	const articles = await Article.find().sort({ createdAt: "desc" });
+	res.render("articles/index", { articles: articles });
+});
+
+app.get("/blog", async (req, res) => {
+	const articles = await Article.find().sort({ createdAt: "desc" });
+	res.render("articles/blog", { articles: articles });
+});
+
+app.get("/thankyou", (req, res) => {
+	res.render("articles/thankyou");
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(3000, () =>
+	console.log("-------- Server Started On Port 3000 --------")
+);
